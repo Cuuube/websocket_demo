3853cb9715752b6d7dc120c5b6ac84d63180f70a
Sater Zhang <zxod@foxmail.com>
2017-05-24 10:36:14 +0800

    throw away useless code

---
+++ server/_httpServer.js
@@ -0,0 +1,12 @@
+// const http = require('http');
+// const path = require('path');
+// const {
+//     port,
+// } = require('./config');
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200,{'Content-Type': 'text/plain'});
+//     res.write('Hello');
+//     res.end();
+// }).listen(port);
+// console.log('server running at: http://0.0.0.0:'+ port);
